bc616d1da3ab7d503877ae3682f3013562c2af44
Anita Platzi <nikosantis+anita@gmail.com>
2019-11-11 16:35:27 -0300

    cambios previos

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import App from './components/App';
+import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Badges />, container);
