419ce9e8a70a4d5462a6719a9fd2188f501680a5
Nikolas Santis <nikosantis@gmail.com>
2019-11-11 00:12:59 -0300

    Cambios para mi nombre

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -6,30 +6,25 @@ import confLogo from '../images/badge-header.svg';
 class Badge extends React.Component {
   render() {
     return (
-      <div className="Badge">
-        <div className="Badge__header">
-          <img src={confLogo} alt="Logo de la conferencia" />
+      <div className='Badge'>
+        <div className='Badge__header'>
+          <img src={confLogo} alt='Logo de la conferencia'></img>
         </div>
 
-        <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
-            alt="Avatar"
-          />
-          <h1>
-            Richard <br /> Kaufman
-          </h1>
+        <div className='Badge__section-name'>
+          <img className='Badge__avatar' src='https://secure.gravatar.com/avatar/75f0189ccbb9cb72d6f0d535b5ab8efe' alt='Avatar' />
+          <h1>Nikolas <br/> Santis</h1>
         </div>
 
-        <div className="Badge__section-info">
-          <h3>Frontend Engineer</h3>
-          <div>@sparragus</div>
+        <div className='Badge__section-info'>
+          <h3>Frontend Developer</h3>
+          <div>@nikosantise</div>
         </div>
-
-        <div className="Badge__footer">#platziconf</div>
+        <div className='Badge__footer'>
+          #platziconf
+        </div>
       </div>
-    );
+    )
   }
 }
 
