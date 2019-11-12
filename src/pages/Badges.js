c0506aa9c4eadae3608311090788355771553a67
Nikolas Santis <nikosantis@gmail.com>
2019-11-12 18:10:26 -0300

    iniial

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
-import PageLoading from '../components/PageLoading';
-import PageError from '../components/PageError';
+
 import api from '../api';
 
 class Badges extends React.Component {
+
   state = {
     loading: true,
     error: null,
-    data: undefined,
+    data: undefined
   };
 
-  componentDidMount() {
+  componentDidMount () {
     this.fetchData();
   }
 
@@ -32,35 +32,40 @@ class Badges extends React.Component {
 
   render() {
     if (this.state.loading === true) {
-      return <PageLoading />;
+      return 'Loading ...';
     }
 
     if (this.state.error) {
-      return <PageError error={this.state.error} />;
+      return `Error ${this.state.error.message}`;
     }
 
     return (
       <React.Fragment>
-        <div className="Badges">
-          <div className="Badges__hero">
-            <div className="Badges__container">
+
+        <div className='Badges'>
+          <div className='Badges__hero'>
+            <div className='Badges__container'>
               <img
-                className="Badges_conf-logo"
+                className='Badges__conf-logo'
                 src={confLogo}
-                alt="Conf Logo"
+                alt='Conf Logo'
               />
             </div>
           </div>
         </div>
 
-        <div className="Badges__container">
-          <div className="Badges__buttons">
-            <Link to="/badges/new" className="btn btn-primary">
+        <div className='Badge__container'>
+          <div className='Badges__buttons'>
+            <Link to='/badges/new' className='btn btn-primary'>
               New Badge
             </Link>
           </div>
 
-          <BadgesList badges={this.state.data} />
+          <div className='Badges__list'>
+            <div className='Badges__container'>
+              <BadgesList badges={this.state.data} />
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
