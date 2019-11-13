5d9d07efbc921e47b813fbf9a26fe1c6336707f7
Nikolas Santis <nikosantis@gmail.com>
2019-11-12 22:46:19 -0300

    cambios finales

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -23,7 +23,7 @@ class Badges extends React.Component {
   }
 
   componentWillUnmount() {
-    clearInterval(this.intervalId);
+    clearInterval(this.intervalId );
   }
 
   fetchData = async () => {
