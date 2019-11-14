163e4555e05dace45315c3279acf2cbfab455f92
Nikolas Santis <nikosantis@gmail.com>
2019-11-13 22:19:08 -0300

    inicial

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles/Modal.css';
-
-function Modal(props) {
-  if (!props.isOpen) {
-    return null;
-  }
-
+function Modal (props) {
   return ReactDOM.createPortal(
-    <div className="Modal">
-      <div className="Modal__container">
-        <button onClick={props.onClose} className="Modal__close-button">
-          X
-        </button>
-
-        {props.children}
-      </div>
-    </div>,
+    <h1>Modal</h1>,
     document.getElementById('modal')
-  );
+  )
 }
 
 export default Modal;
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -4,67 +4,50 @@ import { Link } from 'react-router-dom';
 import './styles/BadgeDetails.css';
 import confLogo from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
-import DeleteBadgeModal from '../components/DeleteBadgeModal';
+import Modal from '../components/Modal';
 
-function BadgeDetails(props) {
+function BadgeDeatils (props) {
   const badge = props.badge;
-
   return (
     <div>
-      <div className="BadgeDetails__hero">
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <img src={confLogo} alt="Logo de la Conferencia" />
+      <div className='BadgeDetails__hero'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-6'>
+              <img src={confLogo} alt='Logo de la Conferencia' />
             </div>
-            <div className="col-6 BadgeDetails__hero-attendant-name">
-              <h1>
-                {badge.firstName} {badge.lastName}
-              </h1>
+            <div className='col-6 BadgeDetails__hero-attendant-name'>
+              <h1>{badge.firstName} {badge.lastName}</h1>
             </div>
           </div>
         </div>
       </div>
-
-      <div className="container">
-        <div className="row">
-          <div className="col">
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
             <Badge
-              firstName={badge.firstName}
-              lastName={badge.lastName}
-              email={badge.email}
-              twitter={badge.twitter}
-              jobTitle={badge.jobTitle}
+            firsName={badge.firsName}
+            lastName={badge.lastName}
+            email={badge.email}
+            twitter={badge.twitter}
+            jobTitle={badge.jobTitle}
             />
           </div>
-          <div className="col">
+          <div className='col'>
             <h2>Actions</h2>
             <div>
-              <div>
-                <Link
-                  className="btn btn-primary mb-4"
-                  to={`/badges/${badge.id}/edit`}
-                >
-                  Edit
-                </Link>
-              </div>
+              <Link className='btn btn-primary mb-4' to={`/badges/${badge.id}/edit`}>Edit</Link>
+            </div>
 
-              <div>
-                <button onClick={props.onOpenModal} className="btn btn-danger">
-                  Delete
-                </button>
-                <DeleteBadgeModal
-                  isOpen={props.modalIsOpen}
-                  onClose={props.onCloseModal}
-                  onDeleteBadge={props.onDeleteBadge}
-                />
-              </div>
+            <div>
+              <button className='btn btn-danger'>Delete</button>
+              <Modal />
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default BadgeDetails;
+export default BadgeDeatils;
