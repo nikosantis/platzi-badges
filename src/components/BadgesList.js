010eb12540d78d6dcdcc86609dcac433f9c49fda
Nikolas Santis <nikosantis@gmail.com>
2019-11-13 23:40:57 -0300

    final

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -27,10 +27,11 @@ class BadgesListItem extends React.Component {
 }
 
 function useSearchBadges(badges) {
-  const [query, setQuery] = React.useState('');
-  const [filteredBadges, setFilteredBadges] = React.useState(badges);
+  const [ query, setQuery ] = React.useState('');
 
-  React.useMemo(() => {
+  const [ filteredBadges, setFilteredBadges ] = React.useState(badges);
+
+  React.useMemo(() =>{
     const result = badges.filter(badge => {
       return `${badge.firstName} ${badge.lastName}`
         .toLowerCase()
@@ -38,12 +39,12 @@ function useSearchBadges(badges) {
     });
 
     setFilteredBadges(result);
-  }, [badges, query]);
+  }, [ badges, query ]);
 
-  return { query, setQuery, filteredBadges };
+  return { query, setQuery, filteredBadges }
 }
 
-function BadgesList(props) {
+function BadgesList (props) {
   const badges = props.badges;
 
   const { query, setQuery, filteredBadges } = useSearchBadges(badges);
@@ -51,18 +52,15 @@ function BadgesList(props) {
   if (filteredBadges.length === 0) {
     return (
       <div>
-        <div className="form-group">
+        <div className='form-group'>
           <label>Filter Badges</label>
-          <input
-            type="text"
-            className="form-control"
+          <input type='text' className='form-control'
             value={query}
-            onChange={e => {
-              setQuery(e.target.value);
+            onChange={(e) => {
+              setQuery(e.target.value)
             }}
           />
         </div>
-
         <h3>No badges were found</h3>
         <Link className="btn btn-primary" to="/badges/new">
           Create new badge
@@ -73,14 +71,12 @@ function BadgesList(props) {
 
   return (
     <div className="BadgesList">
-      <div className="form-group">
+      <div className='form-group'>
         <label>Filter Badges</label>
-        <input
-          type="text"
-          className="form-control"
+        <input type='text' className='form-control'
           value={query}
-          onChange={e => {
-            setQuery(e.target.value);
+          onChange={(e) => {
+            setQuery(e.target.value)
           }}
         />
       </div>
